17ff811125d2033bf79a9b9ce006b343c2b03692
codedbymayank <mayankpal7724@gmail.com>
2025-05-27 14:22:04 +0530

    Lecture - ( What's MVC Pattern)

---
+++ Asp.net_Core_Codes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.net_Core_Codes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
