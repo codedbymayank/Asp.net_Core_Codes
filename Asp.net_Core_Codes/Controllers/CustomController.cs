2fb166e2c8d20cd71456af7441f0b10dd2e5ed06
codedbymayank <mayankpal7724@gmail.com>
2025-06-05 15:43:11 +0530

    Lecture :- ( Controller in Asp.net Core )

---
+++ Asp.net_Core_Codes/Controllers/CustomController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.net_Core_Codes.Controllers
+{
+    public class CustomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string M1()
+        {
+            return "Method M1";
+        }
+
+        //Passing parameters in action method
+        public string getname(string name)
+        {
+            return ("My name is " + name);
+        }
+    }
+}
--- Asp.net_Core_Codes/obj/Debug/net8.0/Asp.net_Core_Codes.AssemblyInfo.cs
+++ Asp.net_Core_Codes/obj/Debug/net8.0/Asp.net_Core_Codes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1672a79942b09206c6b048c7c7e2f5e4f417cd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14b0ee650a5f004798c5b60439b6ab44427864f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
