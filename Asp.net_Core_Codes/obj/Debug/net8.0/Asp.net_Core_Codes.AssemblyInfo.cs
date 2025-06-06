ca6583cbbfe941515a80bf229413feec90fadf4a
codedbymayank <mayankpal7724@gmail.com>
2025-06-06 15:29:17 +0530

    Lecture - ( static files ( img , css , js ) in asp.net core )

--- Asp.net_Core_Codes/obj/Debug/net8.0/Asp.net_Core_Codes.AssemblyInfo.cs
+++ Asp.net_Core_Codes/obj/Debug/net8.0/Asp.net_Core_Codes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e119537e87c7dce9f34d99aca9bb3ec60cca8b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d567b806f83f926aba47d01ae1ba8f09da9e9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
