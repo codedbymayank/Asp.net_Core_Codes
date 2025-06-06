12d89555d72c1244fa56b1e695dfc01ef549e88a
codedbymayank <mayankpal7724@gmail.com>
2025-06-06 11:12:09 +0530

    How Controller finds a view

--- Asp.net_Core_Codes/Controllers/HomeController.cs
+++ Asp.net_Core_Codes/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Asp.net_Core_Codes.Controllers
 
         public ViewResult SomeMethod()
         {
-            // for full path
+            // for full path and we use .cshtml ex6tension also
             return View("TempView/tempview.cshtml");
             //for relative path
             //return View("../../TempView/tempview");
