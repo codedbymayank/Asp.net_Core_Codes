38869df93bce7237631a83c90a87d25d4a9d5ff7
codedbymayank <mayankpal7724@gmail.com>
2025-06-07 11:16:17 +0530

    Lecture - ( Layout in Ap.net Core )

--- Asp.net_Core_Codes/obj/Debug/net8.0/Asp.net_Core_Codes.AssemblyInfo.cs
+++ Asp.net_Core_Codes/obj/Debug/net8.0/Asp.net_Core_Codes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d0676442ec6420b9e9da68d40da9defe1c3ed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8057f2070a877b559d26e2792c34806b3c1d8deb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
