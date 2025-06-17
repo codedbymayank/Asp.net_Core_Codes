1ddad725a7fc366b27572cbdd153b30e80be13b6
codedbymayank <mayankpal7724@gmail.com>
2025-06-17 14:16:55 +0530

    Lecture - ( Override existing html tag )

---
+++ Asp.net_Core_Codes/Helper/BigTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Asp.net_Core_Codes.Helper
+{
+    [HtmlTargetElement("big")]
+    [HtmlTargetElement(Attributes ="big")]
+
+    public class BigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h3";
+            output.Attributes.RemoveAll("big");
+        }
+    }
+}
--- Asp.net_Core_Codes/obj/Debug/net8.0/Asp.net_Core_Codes.AssemblyInfo.cs
+++ Asp.net_Core_Codes/obj/Debug/net8.0/Asp.net_Core_Codes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd3415fcc509f8e6b5b9983a471a865c57712cda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a2c06d62db7dbadf10c82ec31bd74e15676b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.net_Core_Codes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
